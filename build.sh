6f75933f456ead1af2c3affbf8e170a69e5fad8a
Federico Dolce <psykopear@gmail.com>
2021-01-09 14:45:08 +0100

    Manual intervention 2

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/zsh
-zola build && rm -rf public/bio public/contacts public/research/content public/404.html public/robots.txt public/sitemap.xml
+zola build \
+    && rm -rf public/bio public/contacts public/research/content public/404.html public/robots.txt public/sitemap.xml \
+    && mv public/teaching/index.html public/teaching.html \
+    && mv public/research/index.html public/research.html \
+    && rm -rf public/teaching public/research
+
