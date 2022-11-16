f5a1053939191f68a618a3bcd7f4c2836e62845a
Federico Dolce <psykopear@gmail.com>
2022-11-16 19:35:59 +0100

    Moved to NPM, upgraded everything, removed fluidanimation

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
 
 module.exports = () => {
-  const webpack = (cfg) => {
+  const webpack = (cfg, options) => {
     cfg.module.rules.push({
       test: /\.md$/,
-      loader: "frontmatter-markdown-loader",
-      options: { mode: ["react-component"] },
+      use: [
+        {
+          loader: "frontmatter-markdown-loader",
+          options: { mode: ["react-component"] },
+        }
+      ]
     });
     return cfg;
   };
--- pages/research.js
+++ pages/research.js
@@ -1,25 +1,13 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import FluidAnimation from "react-fluid-animation";
 
 import Section from "../components/section";
 import OptionalUrl from "../components/optionalUrl";
 import Journal from "../components/journal";
 import { attributes, react as Content } from "../content/research.md";
 
-const animationConfig = {
-  textureDownsample: 1,
-  densityDissipation: 0.98,
-  velocityDissipation: 0.99,
-  pressureDissipation: 0.8,
-  pressureIterations: 25,
-  curl: 30,
-  splatRadius: 0.005,
-};
-
 const Research = () => {
   const { papers } = attributes;
-  const [simulation, setSimulation] = useState(false);
   return (
     <div className="wrapper">
       <Section>
@@ -44,7 +32,7 @@ const Research = () => {
                   </>
                 )}
                 <br />
-                {p.links.map((l) => (
+                {p.links && p.links.map((l) => (
                   <span key={l.url}>
                     <Link href={l.url}>{l.name}</Link>
                     <br />
@@ -54,21 +42,6 @@ const Research = () => {
             ))}
           </ol>
         </>
-      </Section>
-      <Section dark>
-        {simulation ? (
-          <>
-            <h3>Click and drag the mouse around</h3>
-            <FluidAnimation
-              config={animationConfig}
-              style={{ height: "500px" }}
-            />
-          </>
-        ) : (
-          <div onClick={() => setSimulation(true)} style={{ height: "500px" }}>
-            <h3>Click to activate fluid simulation</h3>
-          </div>
-        )}
       </Section>
     </div>
   );
